0744f769e2876343553d3ce17ba85614ef3a23cd
xxf185 <109097973+xxf185@users.noreply.github.com>
2023-11-19 18:00:57 +0800

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC1091,SC2164,SC2034,SC1072,SC1073,SC1009
 
 # Secure OpenVPN server installer for Debian, Ubuntu, CentOS, Amazon Linux 2, Fedora, Oracle Linux 8, Arch Linux, Rocky Linux and AlmaLinux.
-# https://github.com/angristan/openvpn-install
+# https://github.com/xxf185/openvpn-install
 
 function isRoot() {
 	if [ "$EUID" -ne 0 ]; then
@@ -1180,7 +1180,7 @@ function removeUnbound() {
 
 	until [[ $REMOVE_UNBOUND =~ (y|n) ]]; do
 		echo ""
-		read -rp "确认卸载Unbound? [y/n]: " -e REMOVE_UNBOUND
+		read -rp "卸载Unbound? [y/n]: " -e REMOVE_UNBOUND
 	done
 
 	if [[ $REMOVE_UNBOUND == 'y' ]]; then
@@ -1210,7 +1210,7 @@ function removeUnbound() {
 
 function removeOpenVPN() {
 	echo ""
-	read -rp "确认卸载OpenVPN? [y/n]: " -e -i y REMOVE
+	read -rp "卸载OpenVPN? [y/n]: " -e -i y REMOVE
 	if [[ $REMOVE == 'y' ]]; then
 		# Get OpenVPN port from the configuration
 		PORT=$(grep '^port ' /etc/openvpn/server.conf | cut -d " " -f 2)
